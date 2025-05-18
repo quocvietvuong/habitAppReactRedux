8de6c51ec10d4e399f8bcb79f9f8cc3a064e4a3f
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-05-18 13:33:49 +0700

    Addhabit form and action added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Provider } from "react-redux"
 import './App.css'
 import store from "./store/store"
+import { Container, Typography } from "@mui/material"
+import AddHabitForm from "./components/add-habit-form"
 
 function App() {
   return (
     <Provider store={store}>
-      <div>Hey please subscribe to Vinnie!!!!</div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm/>
+      </Container>
     </Provider>
   )
 }
